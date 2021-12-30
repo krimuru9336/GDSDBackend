fd1a925eaba8474aacdd99d6c50a30351224f282
Kri <kritika.murugan@informatik.hs-fulda.de>
2021-12-30 22:17:44 +0100

    changed to external DB

--- fuldemy/fuldemy/settings.py
+++ fuldemy/fuldemy/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'fuldemy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fuldemydb',
+        'USER': 'root',
+        'PASSWORD': 'kriMuru9336',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
