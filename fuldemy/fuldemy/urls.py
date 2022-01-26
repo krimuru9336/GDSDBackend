d967269b6b8903ec3af6a0cd3bab302c1a5dbaee
Debarati Ray <72045743+stringmad@users.noreply.github.com>
2022-01-26 21:10:19 +0100

    Update urls.py

--- fuldemy/fuldemy/urls.py
+++ fuldemy/fuldemy/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.urls import path,include
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('AllUsers.urls')),
+   ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-]
