9f55adcc7cc57138017e3c2bf7d63b2ffa95ac24
Debarati Ray <72045743+stringmad@users.noreply.github.com>
2022-01-26 21:12:53 +0100

    Update urls.py

--- fuldemy/AllUsers/urls.py
+++ fuldemy/AllUsers/urls.py
@@ -43,4 +43,4 @@ urlpatterns = [
     path('reviews/<int:id>',ActiveClassesView.as_view()),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
