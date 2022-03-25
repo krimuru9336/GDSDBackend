e99c4c1fba491a0d7fb8151274aca87a8c9d52ab
Kri <kritika.murugan@informatik.hs-fulda.de>
2022-03-25 13:09:04 +0100

    Fixed bug for cloud

--- fuldemy/fuldemy/settings.py
+++ fuldemy/fuldemy/settings.py
@@ -96,7 +96,7 @@ ASGI_APPLICATION = 'fuldemy.routing.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
